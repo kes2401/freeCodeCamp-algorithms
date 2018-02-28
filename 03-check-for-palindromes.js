4cfc7704a95040397d171904ba60818b491bc02e
Keith Scully <keithscully83@gmail.com>
2018-02-28 18:16:21 +0000

    add code

--- 03-check-for-palindromes.js
+++ 03-check-for-palindromes.js
@@ -4,3 +4,20 @@
 // You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything lower case in order to check for palindromes.
 // We'll pass strings with varying formats, such as "racecar", "RaceCar", and "race CAR" among others.
 // We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2", and "2_A3*3#A2".
+
+function palindrome(str) {
+  
+  newString = str.replace(/\s/g,'');
+  newString = str.replace(/[^A-Za-z0-9]/g,'');
+  
+  var lowerCaseString = newString.toLowerCase();
+  
+  var flipString = lowerCaseString.split('').reverse().join('');
+  
+  if (lowerCaseString === flipString)
+    return true;
+  else
+    return false;
+}
+
+palindrome("_,race., car..;,");
