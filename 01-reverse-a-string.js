c92b3edd72ed79bfb3054a469e66fe69f5b157e2
Keith Scully <keithscully83@gmail.com>
2018-02-28 18:14:49 +0000

    add code

--- 01-reverse-a-string.js
+++ 01-reverse-a-string.js
@@ -1,3 +1,14 @@
 // Reverse the provided string.
 // You may need to turn the string into an array before you can reverse it.
 // Your result must be a string.
+
+function reverseString(str) {
+  
+  var splitString = str.split("");
+  splitString.reverse();
+  str = splitString.join("");
+  
+  return str;
+}
+
+reverseString("hello");
