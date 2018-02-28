7ed9bf7fadd13da0767c808f4acd4ca678cb494b
Keith Scully <keithscully83@gmail.com>
2018-02-28 18:17:55 +0000

    add code

--- 04-find-the-longest-word-in-a-string.js
+++ 04-find-the-longest-word-in-a-string.js
@@ -1,3 +1,18 @@
 // Return the length of the longest word in the provided sentence.
 // Your response should be a number.
 
+function findLongestWord(str) {
+  
+  var longestString = 0;
+  
+  var words = str.split(" ");
+  
+  for(var i = 0; i < words.length; i++){
+    if (words[i].length > longestString)
+      longestString = words[i].length;
+  }
+    
+  return longestString;
+}
+
+findLongestWord("The quick brown fox jumped over the lazy dog");
