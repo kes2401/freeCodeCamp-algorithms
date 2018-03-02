2d09577c3f84b6cabdffc3ad1e982b9225ef90b6
Keith Scully <keithscully83@gmail.com>
2018-03-02 16:47:17 +0000

    add solution code

--- 08-repeat-a-string-repeat-a-string.js
+++ 08-repeat-a-string-repeat-a-string.js
@@ -1,1 +1,17 @@
 // Repeat a given string (first argument) num times (second argument). Return an empty string if num is not a positive number.
+
+function repeatStringNumTimes(str, num) {
+
+  var newString = "";
+  
+  if (num <= 0) {
+    return "";
+  }
+  else {  
+    newString = str.repeat(num);
+  }
+
+  return newString;
+}
+
+repeatStringNumTimes("abc", 3);
