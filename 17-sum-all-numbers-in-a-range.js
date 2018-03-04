72f3aebdea602c5998580034d09c6672dd32da36
Keith Scully <keithscully83@gmail.com>
2018-03-04 18:34:19 +0000

    add solution code

--- 17-sum-all-numbers-in-a-range.js
+++ 17-sum-all-numbers-in-a-range.js
@@ -1,2 +1,17 @@
 // We'll pass you an array of two numbers. Return the sum of those two numbers and all numbers between them.
 // The lowest number will not always come first.
+
+function sumAll(arr) {
+  
+  var max = Math.max.apply(null, arr);
+  var min = Math.min.apply(null, arr);
+  var sum = min;
+  
+  for(var i = min+1; i <= max; i++){
+    sum += i;
+  }
+  
+  return sum;
+}
+
+sumAll([1, 4]);
