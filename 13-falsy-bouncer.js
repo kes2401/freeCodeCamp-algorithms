647f94f437eea48e6e4742662a524d862b43b23d
Keith Scully <keithscully83@gmail.com>
2018-03-04 18:30:06 +0000

    add solution code

--- 13-falsy-bouncer.js
+++ 13-falsy-bouncer.js
@@ -1,2 +1,11 @@
 // Remove all falsy values from an array.
 // Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+function bouncer(arr) {
+  
+  var newArray = arr.filter(Boolean);
+
+  return newArray;
+}
+
+bouncer([7, "ate", "", false, 9]);
