0ab39b174cb4eced67457a19ad3b34d7c12a95a7
Keith Scully <keithscully83@gmail.com>
2018-03-04 18:29:21 +0000

    add solution code

--- 12-mutations.js
+++ 12-mutations.js
@@ -2,3 +2,21 @@
 // For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
 // The arguments ["hello", "hey"] should return false because the string "hello" does not contain a "y".
 // Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
+
+function mutation(arr) {
+  
+  var lowerCaseArray = [];
+  for (var i = 0; i < arr.length; i++) {
+    lowerCaseArray.push(arr[i].toLowerCase());
+  }
+  
+  for(var j = 0; j < lowerCaseArray[1].length; j++){
+      var val = lowerCaseArray[0].indexOf(lowerCaseArray[1][j]);   
+        console.log(val);
+      if (val == -1)
+        return false;     
+   }
+  return true;
+}
+
+mutation(["hello", "hey"]);
