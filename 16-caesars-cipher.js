a88b84805efba0a525e75c1133024fce912dc47b
Keith Scully <keithscully83@gmail.com>
2018-03-04 18:33:18 +0000

    add solution code

--- 16-caesars-cipher.js
+++ 16-caesars-cipher.js
@@ -2,3 +2,26 @@
 // A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus 'A' ↔ 'N', 'B' ↔ 'O' and so on.
 // Write a function which takes a ROT13 encoded string as input and returns a decoded string.
 // All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
+
+function rot13(str) { // LBH QVQ VG!
+  
+  var decodedString = "";
+ 
+  for(var i = 0; i < str.length; i++){
+      
+      var temp = str.charCodeAt(i);
+    
+      if(temp >= 65 && temp <= 77){
+        temp += 13;
+      } else if (temp >= 78 && temp <= 90){
+        temp -= 13;
+      }
+       
+    decodedString += String.fromCharCode(temp);
+    
+    }
+
+  return decodedString;
+}
+
+rot13("SERR PBQR PNZC");
