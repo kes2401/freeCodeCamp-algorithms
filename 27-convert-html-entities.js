8f0713682786401a378d4ca8f3ca2b6e966abb6c
Keith Scully <keithscully83@gmail.com>
2018-03-04 18:39:26 +0000

    add solution code

--- 27-convert-html-entities.js
+++ 27-convert-html-entities.js
@@ -1,1 +1,15 @@
 // Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+
+function convertHTML(str) {
+  
+  var newStr = str.replace(/&/g, "&amp;");
+  newStr = newStr.replace(/</g, "&lt;");
+  newStr = newStr.replace(/>/g, "&gt;");
+  newStr = newStr.replace(/"/g, "&quot;");
+  newStr = newStr.replace(/'/g, "&apos;");
+  
+  return newStr;                         
+  
+}
+
+convertHTML("Dolce & Gabbana");
