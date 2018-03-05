69e1a80062710a7b94271f00854ac7f22b854448
Keith Scully <keithscully83@gmail.com>
2018-03-05 21:14:39 +0000

    add solution code

--- 32-finders-keepers.js
+++ 32-finders-keepers.js
@@ -1,1 +1,11 @@
 // Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument).
+
+function findElement(arr, func) {
+  
+  var newArray = arr.filter(func);
+  
+  return newArray[0];
+  
+}
+
+findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
