40fdd310abf49112ce2fcdcaa1a979eae03866c1
Keith Scully <keithscully83@gmail.com>
2018-03-05 21:15:42 +0000

    add solution code

--- 35-binary-agents.js
+++ 35-binary-agents.js
@@ -1,2 +1,21 @@
 // Return an English translated sentence of the passed binary string.
 // The binary string will be space separated.
+
+function binaryAgent(str) {
+  
+  var strArr = str.split (' ');
+  
+  var letters =[];
+  
+  for (var i = 0; i < strArr.length; i++){
+    
+    var tempDec = parseInt (strArr[i], 2);
+    var temp = String.fromCharCode(tempDec);
+    letters.push(temp);
+  }
+  
+  var sentence = letters.join('');
+  return sentence;
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
